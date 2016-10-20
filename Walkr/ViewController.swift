864e4e3bda9f49cb1cfd8e72d8f3d3cedd9f2e5d
Awsomebulderguy <jerry@qresort.com>
2016-10-20 15:45:48 -0400

    1st commit

---
+++ Walkr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Walkr
+//
+//  Created by Jerry on 10/18/16.
+//  Copyright © 2016 HMX Roaring Robots. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ok")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
